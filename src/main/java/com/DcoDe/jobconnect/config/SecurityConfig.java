fae8b2175c0b0c5a42e28d05baa446139c7cc5a7
ayushanand593 <ayushanand593@gmail.com>
2025-05-24 15:15:27 +0530

    Revert "csrf property changed"
    This reverts commit 6422efe259563e42dfa813ab25c20a3d35461d8f.

--- src/main/java/com/DcoDe/jobconnect/config/SecurityConfig.java
+++ src/main/java/com/DcoDe/jobconnect/config/SecurityConfig.java
@@ -40,8 +40,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
         .cors(withDefaults())
-            .csrf(csrf -> csrf
-    .ignoringRequestMatchers("/api/**"))
+            .csrf(csrf -> csrf.disable())
             .sessionManagement(session ->
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
